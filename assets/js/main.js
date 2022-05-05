e7ed6ef7c6ccc48bf5e7d82efa3e52fecdccacd0
santiago <santysjr956@gmail.com>
2022-05-05 12:32:43 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,25 @@
+
+ 
+      var swiper = new Swiper('.swiper', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+      
+
+    
+/*document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });*/
+
---
+++ assets/js/tagCanvas.js
@@ -0,0 +1,43 @@
+window.onload = function() {
+        try {
+          TagCanvas.Start('myCanvas','tags',options);
+        } catch(e) {
+          // something went wrong, hide the canvas container
+          document.getElementById('myCanvasContainer').style.display = 'none';
+        }
+      };
+    var options = { 
+    textColour : '#7384BC',
+
+    outlineThickness : 0.5,
+    outlineColour : '#fe0853',
+    maxSpeed : 0.06,
+    freezeActive:true,
+    shuffleTags:true,
+    shape:'sphere',
+    zoom:1,
+    noSelect:true,
+    textFont:'arial',
+    textHeight:'20',
+
+    pinchZoom:true,
+    freezeDecel:true,
+    fadeIn:3000,
+    initial: [0.3,-0.1],
+    depth : 0.8
+  }
+
+
+  
+
+
+
+ 
+
+
+
+
+
+
+
+
